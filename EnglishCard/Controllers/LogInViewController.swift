79c70994cbfb2f357f376ed0d38700e7a3ccbd72
onur.basdas <onur.basdas@3pounds.io>
2021-09-08 15:24:54 +0300

    password hide completed

--- EnglishCard/Controllers/LogInViewController.swift
+++ EnglishCard/Controllers/LogInViewController.swift
@@ -13,6 +13,7 @@ class LogInViewController: UIViewController {
     
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet var lockButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +26,9 @@ class LogInViewController: UIViewController {
         emailText.layer.borderWidth = 1.0
         passwordText.layer.borderColor = myColor.cgColor
         passwordText.layer.borderWidth = 1.0
+        lockButton.tintColor = myColor
+        passwordText.isSecureTextEntry = true
+
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -44,5 +48,16 @@ class LogInViewController: UIViewController {
     @IBAction func signUpClicked(_ sender: Any) {
         getSignUp()
     }
+    
+    @IBAction func passwordHideClicked(_ sender: Any) {
+        if passwordText.isSecureTextEntry == true {
+            lockButton.setImage(UIImage(systemName: "lock.fill"), for: .normal)
+        } else {
+            lockButton.setImage(UIImage(systemName: "lock"), for: .normal)
+        }
+        passwordText.isSecureTextEntry = !passwordText.isSecureTextEntry
+    }
+    
+    
 }
 
